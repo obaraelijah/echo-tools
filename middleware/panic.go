6d99852210909dd8da6cf49c83a94e73705fd934
obaraelijah <elijahobara357@gmail.com>
2025-04-28 10:52:13 +0300

    Added panic middleware

---
+++ middleware/panic.go
@@ -0,0 +1,40 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+
+	"github.com/labstack/echo/v4"
+	"github.com/obaraelijah/echo-tools/logging"
+)
+
+func Panic(log logging.Logger) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			defer func() {
+				if r := recover(); r != nil {
+					if r == http.ErrAbortHandler {
+						panic(r)
+					}
+					err, ok := r.(error)
+					if !ok {
+						err = fmt.Errorf("%v", r)
+					}
+
+					stack := make([]byte, 4<<10) // 4 KB Stack size
+					var length int
+					if logging.GetLogLevel() == logging.DEBUG {
+						length = runtime.Stack(stack, true)
+					} else {
+						length = runtime.Stack(stack, false)
+					}
+					stack = stack[:length]
+
+					log.Errorf("[PANIC RECOVER] %v %s\n", err, stack[:length])
+				}
+			}()
+			return next(c)
+		}
+	}
+}
