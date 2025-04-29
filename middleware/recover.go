b26e47d263dfe7c91f04fb8bb1c783b6a0fe40c2
obaraelijah <elijahobara357@gmail.com>
2025-04-29 13:45:46 +0300

    Replaced own logger with echo log

--- middleware/recover.go
+++ middleware/recover.go
@@ -6,14 +6,13 @@ import (
 	"runtime"
 
 	"github.com/labstack/echo/v4"
-	"github.com/obaraelijah/echo-tools/logging"
+	"github.com/labstack/gommon/log"
 )
 
 func Panic() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			defer func() {
-				log := logging.GetLogger("panic-mw")
 				if r := recover(); r != nil {
 					if r == http.ErrAbortHandler {
 						panic(r)
@@ -25,7 +24,7 @@ func Panic() echo.MiddlewareFunc {
 
 					stack := make([]byte, 4<<10) // 4 KB Stack size
 					var length int
-					if logging.GetLogLevel() == logging.DEBUG {
+					if log.Level() == log.DEBUG {
 						length = runtime.Stack(stack, true)
 					} else {
 						length = runtime.Stack(stack, false)
