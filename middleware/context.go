e1849faca4d841964513165ab5ac82509fa62a82
obaraelijah <elijahobara357@gmail.com>
2025-04-29 13:44:40 +0300

    chore: Removed customcontext as it has to be defined by user

--- middleware/context.go
+++ middleware/context.go
@@ -1,8 +1,6 @@
 package middleware
 
 import (
-	"reflect"
-
 	"github.com/labstack/echo/v4"
 )
 
@@ -11,15 +9,5 @@ func Wrap[T any](f func(cc T) error) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		cc := c.(T)
 		return f(cc)
-	}
-}
-
-// CustomContext Requires a pointer to a struct that embeds echo.Context or has a field with the name Context
-func CustomContext(cc any) echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			reflect.ValueOf(cc).Elem().FieldByName("Context").Set(reflect.ValueOf(&c).Elem())
-			return next(cc.(echo.Context))
-		}
 	}
 }
