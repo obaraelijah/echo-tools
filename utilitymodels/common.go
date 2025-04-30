9a57e673dc40e5aaa3b848ed780f81474b7c6437
obaraelijah <elijahobara357@gmail.com>
2025-04-30 11:31:47 +0300

    chore: Removed CommonSoftDelete from utilitymodels

--- utilitymodels/common.go
+++ utilitymodels/common.go
@@ -2,8 +2,6 @@ package utilitymodels
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type CommonID struct {
@@ -14,11 +12,4 @@ type Common struct {
 	ID        uint      `gorm:"primarykey" json:"id"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
-}
-
-type CommonSoftDelete struct {
-	ID        uint           `gorm:"primarykey" json:"id"`
-	CreatedAt time.Time      `json:"-"`
-	UpdatedAt time.Time      `json:"-"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
